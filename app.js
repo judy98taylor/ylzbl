53c64d0f7aba0224d56005ee2a0bd7f46a4d14cc
luckyzxd <luckyzxd@mi-20160909nyqg.mioffice.cn>
2017-12-08 11:49:54 +0800

    -s
    -s

---
+++ app.js
@@ -0,0 +1,11 @@
+var http = require("http");
+var express = require("express");
+var app=express();
+app.use(express.static('public'));
+app.get('/', function(req, res){
+    res.send('hello world');
+  });
+
+var serve=app.listen(8888, ()=>{
+    console.log('listen 8888');
+});
