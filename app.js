3ea171f4db1a8e1222cca7e846ef25189b4355f9
luckyzd <zhangdi5@xiaomi.com>
2017-12-08 12:21:15 +0800

    a

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var http = require("http");
 var express = require("express");
-var app=express();
+var app = express();
 app.use(express.static('public'));
-app.get('/', function(req, res){
-    res.send('hello world');
-  });
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
 
-var serve=app.listen(8888, ()=>{
-    console.log('listen 8888');
+var serve = app.listen(8888, () => {
+  console.log('listen 8888');
 });
