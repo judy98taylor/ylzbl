c1808ece082363fdc7d499f1200bf86d92a20a2d
luckyzd <zhangdi5@xiaomi.com>
2017-12-09 19:18:00 +0800

    -save

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = express();
 var router = express.Router();
 var APPID = 'wx43d17d7d013dc48f';
 var APPSECRET = 'e90a6f9769f8748cb164f4282ada0295';
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.send(`<!DOCTYPE html>
   <html lang="en">
   
@@ -37,13 +37,13 @@ router.get('/', function(req, res) {
   `);
 });
 
-router.get('/get', function(req, res) {
+router.get('/get', function (req, res) {
 
   // req.query get
   // req.params post
 
   var obj = { a: 1, b: 2, c: 3 };
-  request.get(`http://nodejs.org/dist/index.json?${querystring.stringify(obj)}`, function(error, response, body) {
+  request.get(`http://nodejs.org/dist/index.json?${querystring.stringify(obj)}`, function (error, response, body) {
     // console.log('error:', error); // Print the error if one occurred
     // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     // console.log('body:', body); // Print the HTML for the Google homepage.
@@ -67,7 +67,7 @@ router.get('/get_access_token', (req, res) => {
 app.use('/wx', router);
 app.use('/wx', express.static('wx'));
 
-var server = app.listen(8888, function() {
+var server = app.listen(8888, function () {
   var host = server.address().address;
   var port = server.address().port;
 
